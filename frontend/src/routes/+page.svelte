cccbc684a82a92f611ec43f0dad7b803b9cbdbb7
JaylenLuc <jaylenluc1@gmail.com>
2025-01-28 00:48:16 +0000

    1/3 of the basic UI and none of the functionality yet

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script> 
+    import {Button} from '../lib/components/ui/button';
+    import { Input } from '../lib/components/ui/input';
+</script>
+
+<main>
+    <div class="flex items-center justify-center h-64 bg-gray-200">
+        <div class="w-1/2 p-6 bg-gray-100 rounded-lg">
+            <Input placeholder="enter your order request"/> 
+        </div>
+        <Button class="w-[80px] h-[80px]">Run</Button>
+    </div>
+</main>
